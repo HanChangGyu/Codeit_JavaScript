8371c921d6570dd341844f30c3948028ff72a4ec
HanChangGyu <ceu0723@gmail.com>
2026-01-12 15:59:05 +0900

    feat:조건문 예제 추가

--- 2_제어문/1_If.js
+++ 2_제어문/1_If.js
@@ -35,3 +35,48 @@ if (temperature2 <= 0) {
 } else {
     console.log('물이 모두 수증기가 되었습니다.');
 }
+
+console.log('');
+
+// 예제 
+// 나의 나이 및 성별
+let myAge = 26;
+let myGender = 'male';
+
+// 호칭 변수
+let callOlderBrother = '형';
+let callOlderSister = '누나';
+let callFriend = '친구';
+let callYoungerSister = '여동생';
+let callYoungerBrother = '남동생';
+
+
+function whatShouldICallYou(yourAge, yourGender) {
+  if (myAge === yourAge) {
+    return callFriend;
+  } else if (myAge > yourAge) {
+    if (yourGender === 'male') {
+      return callYoungerBrother;
+    } else if (yourGender === 'female') {
+      return callYoungerSister;
+    }
+  } else {
+    if (yourGender === 'male') {
+      return callOlderBrother;
+    } else if (yourGender === 'female'){
+      return callOlderSister;
+    }
+  }
+}
+
+let result1 = whatShouldICallYou(25, 'female');
+let result2 = whatShouldICallYou(20, 'male');
+let result3 = whatShouldICallYou(26, 'female');
+let result4 = whatShouldICallYou(30, 'male');
+let result5 = whatShouldICallYou(31, 'female');
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
