390a48e523d9ee0e4828df71f6590762b077e5fb
HanChangGyu <ceu0723@gmail.com>
2026-01-19 19:20:05 +0900

    feat: 자료형 심화 숫자 표기법

---
+++ 5_자료형_심화/1_숫자.js
@@ -0,0 +1,25 @@
+// 숫자 표기법
+
+// 지수 표기법
+console.log(25e9 === 2500000); // * 10의 거듭제곱
+console.log(5.3e3 === 5300);
+console.log(-6.1e8 === -610000000);
+
+console.log(16e-5 === 0.00016);
+console.log(3.5e3 === 0.0035);
+console.log(-9.1e-5 === 0.000091);
+
+// 16진법 (Hexadecimal)
+let hex1 = 0xff; // 255 x사용
+let hex2 = 0xFF // 255
+
+// 8진법 (Octal)
+let octal = 0o377; // 255 o사용
+
+// 2진법 (binary numeral system)
+let binary = 0b11111111; // 255 b사용
+
+console.log(hex1);
+console.log(hex2);
+console.log(octal);
+console.log(binary);
